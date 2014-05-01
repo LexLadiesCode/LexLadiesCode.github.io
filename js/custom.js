003554febddbeca9ace271d579f2a7b447dab271
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-04-30 21:14:58 -0400

    Style posts lists, add moment.js for relative time

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,8 @@
 $(function() {
   $('[data-toggle="tooltip"]').tooltip();
+  $('time').each(function() {
+    var time_tag = $(this);
+    var time_str = time_tag.text();
+    time_tag.text(moment(time_str).fromNow());
+  });
 });
