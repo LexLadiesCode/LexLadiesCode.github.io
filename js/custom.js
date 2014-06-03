8cc7194a2f74103eb7d55f5108e2f1f0e1c52353
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-06-03 18:42:38 -0400

    Smooth scroll to top, colored sections of page

--- js/custom.js
+++ js/custom.js
@@ -9,16 +9,23 @@ $(function() {
     var time_str = time_tag.text();
     time_tag.text(moment(time_str).fromNow());
   });
-  $('.navbar-nav a').click(function(event) {
+  $('a[href^=#]').click(function(event) {
     var link = $(this);
     var url = link.attr('href');
-    if (url.indexOf('#') !== 0) { // Not an anchor tag
-      return true;
-    }
     event.preventDefault();
-    var y_offset = $(url).offset().top - $('.navbar-fixed-top').height();
+    var area_to_scroll_to = $(url);
+    if (area_to_scroll_to.length < 1) {
+      var name = url.substr(1); // #top-of-page => top-of-page
+      area_to_scroll_to = $('[name="' + name + '"]');
+    }
+    var y_offset = area_to_scroll_to.offset().top -
+                   $('.navbar-fixed-top').height();
     $('html, body').animate({scrollTop: y_offset}, 500, function() {
-      window.location.hash = url;
+      if (url === '#top-of-page') {
+        window.location.hash = '';
+      } else {
+        window.location.hash = url;
+      }
     });
     return false;
   });
